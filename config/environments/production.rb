69cd03fb380bb30c3693ed5198633845d12b91c7
shu0115 <s.matsumoto0115@gmail.com>
2013-12-19 19:54:23 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ WeWantTokyoRegion::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # メール設定
+  config.action_mailer.default_url_options   = { host: 'we-want-tokyo-region.herokuapp.com' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings         = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            "rails.dev0115@gmail.com",
+    password:             ENV['MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+  }
 end
