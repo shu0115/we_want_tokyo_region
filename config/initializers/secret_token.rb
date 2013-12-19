61ab5eb49cf9a8cde5be4e23624d8d706aecf562
shu0115 <s.matsumoto0115@gmail.com>
2013-12-19 19:45:54 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeWantTokyoRegion::Application.config.secret_key_base = 'c8159f9f8904469de9d7c9e0f5be916bc9b394c40091725a5b34024703884df9daa358019995a36a1beedfccb767f45ccbdf939af8003c305e0ac266361056bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeWantTokyoRegion::Application.config.session_store :cookie_store, key: '_we_want_tokyo_region_session'
